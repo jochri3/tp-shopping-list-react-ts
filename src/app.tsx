43f149dab5d19a87db7816cbd5c678904ff1ee3b
Christian Lisangola <jochri3@gmail.com>
2021-12-09 12:28:52 +0300

    Initial commit

---
+++ src/app.tsx
@@ -0,0 +1,5 @@
+import ShoppingList from "./shopping-list";
+
+export default function App() {
+  return <ShoppingList title="Liste de courses" />;
+}
---
+++ src/shopping-list.tsx
@@ -0,0 +1,5 @@
+const ShoppingList = () => {
+  return null;
+};
+
+export default ShoppingList;
